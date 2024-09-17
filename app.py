0438c3f1c3661e31c42dcc31de4908dc64032565
itsnicholas <38068987+itsnicholas@users.noreply.github.com>
2024-09-17 18:09:21 +0300

    Protected login and signup from SQL injection

--- app.py
+++ app.py
@@ -19,22 +19,31 @@ def login():
     username = request.form["username"]
     password = request.form["password"]
 
-    result = db.session.execute(text("SELECT id, password FROM users WHERE username = '" +
-                                     username + "';"))
+    if len(username) > 16:
+        return render_template("index.html", error="Tunnus on liian pitkä")
+    if len(username) < 1:
+        return render_template("index.html", error="Tunnus on liian lyhyt")
+    if len(password) > 16:
+        return render_template("index.html", error="Salasana on liian pitkä")
+    if len(password) < 8:
+        return render_template("index.html", error="Salasana on liian lyhyt")
+
+    sql = text("SELECT id, password FROM users WHERE username =:username;")
+    result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
 
     if not user:
         # invalid username
         return render_template("index.html", message="Käyttäjätunnus on väärä")
     else:
-        # correct username and password
         hash_value = user.password
+        # correct username and password
         if check_password_hash(hash_value, password):
             session["username"] = username
             return redirect("/")
-        else:
-            # invalid password
-            return render_template("index.html", message="Salasana on väärä")
+
+        # invalid password
+        return render_template("index.html", message="Salasana on väärä")
 
 @app.route("/logout")
 def logout():
@@ -50,19 +59,27 @@ def signup2():
     username = request.form["username"]
     password = request.form["password"]
 
-    result = db.session.execute(text("SELECT id FROM users WHERE username = '" +
-                                     username + "';"))
-    print("result", result)
+    if len(username) > 16:
+        return render_template("signup.html", error="Tunnus on liian pitkä")
+    if len(username) < 1:
+        return render_template("signup.html", error="Tunnus on liian lyhyt")
+    if len(password) > 16:
+        return render_template("signup.html", error="Salasana on liian pitkä")
+    if len(password) < 8:
+        return render_template("signup.html", error="Salasana on liian lyhyt")
+
+    sql = text("SELECT id, password FROM users WHERE username =:username;")
+    result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
 
     if user:
-        # username is in use
-        return render_template("signup.html", message="Käyttäjätunnus on käytössä - kokeile toista käyttäjätunnusta")
-    else:
-        # store username and password
-        hash_value = generate_password_hash(password)
-        db.session.execute(text("INSERT INTO users VALUES (DEFAULT, '" +
-                                username + "', '" + hash_value + "', false);"))
-        db.session.commit()
+        return render_template("signup.html", message="Käyttäjätunnus on käytössä" +
+                               " - kokeile toista käyttäjätunnusta")
+
+    # store username and password
+    hash_value = generate_password_hash(password)
+    sql = text("INSERT INTO users VALUES (DEFAULT, :username, :hash_value, false);")
+    db.session.execute(sql, {"username":username, "hash_value":hash_value})
+    db.session.commit()
 
     return redirect("/")
